45161c45869c6ef24b5296ff370fb7519a1fa4ce
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-07-02 03:45:50 -0700

    base, home: demo with 'windfield' physics engine

--- main.lua
+++ main.lua
@@ -1,34 +1,32 @@
+--local imgPath = basePath ..
+--        '/assets/tf_animals/tf_animals/individual_frames/animals1/animals1_34.png'
+--local imgBytes = network.request(imgPath)
+--local imgFileData = love.filesystem.newFileData(imgBytes, 'animals1_34.png')
+--local imgData = love.image.newImageData(imgFileData)
+--local img = love.graphics.newImage(imgData)
+
 local wf = require 'https://raw.githubusercontent.com/SSYGEN/windfield/master/windfield/init.lua'
 
-local basePath = portal.path:gsub('/main.lua$', '')
+function love.load()
+    world = wf.newWorld(0, 0, true)
+    world:setGravity(0, 512)
 
-local x = portal.args.x
-local y = portal.args.y
+    box = world:newRectangleCollider(400 - 50/2, 0, 50, 50)
+    box:setRestitution(0.8)
+    box:applyAngularImpulse(5000)
 
-local children = {}
-
-local imgPath = basePath ..
-        '/assets/tf_animals/tf_animals/individual_frames/animals1/animals1_34.png'
-local imgBytes = network.request(imgPath)
-local imgFileData = love.filesystem.newFileData(imgBytes, 'animals1_34.png')
-local imgData = love.image.newImageData(imgFileData)
-local img = love.graphics.newImage(imgData)
-
-function love.draw()
-    love.graphics.push('all')
-    love.graphics.draw(img, x, y)
-    love.graphics.pop()
-
-    for _, child in pairs(children) do
-        child:draw()
-    end
+    ground = world:newRectangleCollider(0, 550, 800, 50)
+    wall_left = world:newRectangleCollider(0, 0, 50, 600)
+    wall_right = world:newRectangleCollider(750, 0, 50, 600)
+    ground:setType('static') -- Types can be 'static', 'dynamic' or 'kinematic'. Defaults to 'dynamic'
+    wall_left:setType('static')
+    wall_right:setType('static')
 end
 
-function love.mousepressed()
-    if portal.args.spawnChildren then
-        table.insert(children, portal:newChild(portal.path, {
-            x = love.graphics.getWidth() * math.random(),
-            y = love.graphics.getHeight() * math.random(),
-        }))
-    end
+function love.update(dt)
+    if world then world:update(dt) end
+end
+
+function love.draw()
+    if world then world:draw() end
 end
