af7eb8c9bfd10220684dd1456d65f3526fbc812b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-07-04 01:46:59 -0700

    home: start making this more of a game

---
+++ assets/tilesets/dark.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dark" tilewidth="16" tileheight="16" tilecount="609" columns="29">
+ <image source="dark.png" width="464" height="336"/>
+</tileset>
--- main.lua
+++ main.lua
@@ -1,8 +1,27 @@
-function love.load()
-end
+-- Load libs directly from their GitHub URIs!
 
-function love.update(dt)
+local sti = require 'https://raw.githubusercontent.com/karai17/Simple-Tiled-Implementation/master/sti/init.lua'
+local moonshine = require 'https://raw.githubusercontent.com/vrld/moonshine/master/init.lua'
+
+local map
+local TILE_SIZE = 16
+
+local effect
+
+function love.load()
+    map = sti('assets/maps/map1.lua')
+    effect = moonshine(moonshine.effects.filmgrain).chain(moonshine.effects.vignette)
+    effect.filmgrain.size = 2
 end
 
 function love.draw()
+    effect(function()
+        love.graphics.push('all')
+        map:draw(-49 * TILE_SIZE, -54 * TILE_SIZE, 2, 2)
+        love.graphics.pop()
+    end)
+end
+
+function love.update(dt)
+    map:update(dt)
 end
