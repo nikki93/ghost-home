a5443fbdc2c3ecffd8ddef4560154f8a4655132b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-07-04 21:43:40 -0700

    formatting

--- core/init.lua
+++ core/init.lua
@@ -1,14 +1,19 @@
 core = {}
 
-jit.off() -- Use non-FFI versions of CPML
-core.vec2 = require 'https://raw.githubusercontent.com/excessive/cpml/master/modules/vec2.lua'
-core.color = require 'https://raw.githubusercontent.com/excessive/cpml/master/modules/color.lua'
---core.vec3 = cpml.vec3
---core.quat = cpml.quat
---core.bound2 = cpml.bound2
---core.bound3 = cpml.bound3
-jit.on()
+do
+    jit.off() -- Use non-FFI versions of CPML
 
+    core.vec2 = require 'https://raw.githubusercontent.com/excessive/cpml/130fe2aca042b39299d16bd63df3aa606fe630ad/modules/vec2.lua'
+    core.color = require 'https://raw.githubusercontent.com/excessive/cpml/130fe2aca042b39299d16bd63df3aa606fe630ad/modules/color.lua'
+
+    -- Skip 3d math stuff for now
+    --core.vec3 = cpml.vec3
+    --core.quat = cpml.quat
+    --core.bound2 = cpml.bound2
+    --core.bound3 = cpml.bound3
+
+    jit.on()
+end
 
 require 'core.entity'
 require 'core.spatial'
