a8a6e44a8181be9ece4022d6509004f7a92ecd64
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-07-04 19:46:42 -0700

    base, home: start working on `entity` component-based entity library

--- main.lua
+++ main.lua
@@ -1,26 +1,107 @@
--- Load libs directly from their GitHub URIs!
+local Spatial = entity.newComponentType('Spatial')
 
-local sti = require 'https://raw.githubusercontent.com/karai17/Simple-Tiled-Implementation/master/sti/init.lua'
-local moonshine = require 'https://raw.githubusercontent.com/nikki93/moonshine/master/init.lua'
+local defaultX, defaultY = 0.5 * love.graphics.getWidth(), 0.5 * love.graphics.getHeight()
 
-local map
-local TILE_SIZE = 16
+function Spatial:add()
+    self.x, self.y = defaultX, defaultY
+    self.width, self.height = 32, 32
+    self.rotation = 0
+end
 
-local effect
 
-function love.load()
-    map = sti('assets/maps/map1.lua')
-    effect = moonshine(moonshine.effects.vignette)
+local Visual = entity.newComponentType('Visual')
+
+-- Maintain references to all `Visual` instances as keys
+local all = {}
+
+-- Order to draw `Visual` instances in as an array -- `orderDirty` is whether it needs an update
+local order = {}
+local orderDirty = true
+
+function Visual:add()
+    self._depth = 1
+
+    -- Names of visual component types attached to this entity
+    self.dependentTypes = {}
+
+    all[self] = true
+    orderDirty = true
+end
+
+function Visual:remove()
+    all[self] = nil
+    orderDirty = true
+end
+
+function Visual:setDepth(newDepth)
+    self._depth = newDepth
+    orderDirty = true
+end
+
+function Visual:getDepth()
+    return self._depth
+end
+
+function Visual:addDependent(dependentType)
+    self.dependentTypes[dependentType] = true
+end
+
+function Visual:removeDependent(dependentType)
+    self.dependentTypes[dependentType] = false
 end
 
 function love.draw()
-    effect(function()
-        love.graphics.push('all')
-        map:draw(-49 * TILE_SIZE, -54 * TILE_SIZE, 2, 2)
-        love.graphics.pop()
-    end)
+    -- Update `order` if it's dirty
+    if orderDirty then
+        order = {}
+        for instance in pairs(all) do
+            table.insert(order, instance)
+        end
+        table.sort(order, function(i1, i2)
+            return i1._depth < i2._depth
+        end)
+    end
+
+    -- Render all visual component types for every visual entity
+    for _, instance in ipairs(order) do
+        for dependentType in pairs(instance.dependentTypes) do
+            local dependentInstance = instance.ent[dependentType]
+            if dependentInstance.draw then
+                dependentInstance:draw()
+            end
+        end
+    end
 end
 
-function love.update(dt)
-    map:update(dt)
+
+local Sprite = entity.newComponentType('Sprite', {
+    depends = { 'Spatial', 'Visual' },
+})
+
+local defaultImage = love.graphics.newImage('assets/avatar2.png')
+
+function Sprite:add()
+    self.image = defaultImage
+    self.scale = 1
 end
+
+function Sprite:draw()
+    local spatial = self.Spatial
+    love.graphics.draw(self.image,
+        spatial.x, spatial.y, spatial.rotation,
+        self.scale, self.scale,
+        spatial.width / 2, spatial.height / 2)
+end
+
+
+local ent1 = entity.newEntity()
+ent1:addComponent('Sprite')
+
+local ent2 = entity.newEntity()
+ent2:addComponent('Sprite')
+ent2.Spatial.x, ent2.Spatial.y = defaultX + 64, defaultY + 64
+
+function love.update(dt)
+    ent2.Visual:setDepth(math.floor(love.timer.getTime()) % 2 == 0 and 2 or 0)
+end
+
