7d41bbabb2c3ef2b8d8010a36cc6429904b5c29a
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-07-04 22:25:36 -0700

    visual: only re-fix draw order array once...

--- core/visual.lua
+++ core/visual.lua
@@ -6,12 +6,24 @@ local all = {}
 -- Order to draw `Visual` instances in as an array -- `orderDirty` is whether it needs an update
 local order = {}
 local orderDirty = true
+local function ensureOrder()
+    if orderDirty then
+        order = {}
+        for instance in pairs(all) do
+            table.insert(order, instance)
+        end
+        table.sort(order, function(i1, i2)
+            return i1._depth < i2._depth
+        end)
+        orderDirty = false
+    end
+end
 
 function Visual:add()
     self._depth = 1
 
-    -- Names of visual component types attached to this entity
-    self.dependentTypes = {}
+    -- Visual component instances attached to this entity as keys
+    self.visuals = {}
 
     all[self] = true
     orderDirty = true
@@ -32,32 +44,24 @@ function Visual:getDepth()
 end
 
 function Visual:addDependent(dependentType)
-    self.dependentTypes[dependentType] = true
+    local dependentInstance = self.ent[dependentType]
+    if dependentInstance.draw then
+        self.visuals[dependentInstance] = true
+    end
 end
 
 function Visual:removeDependent(dependentType)
-    self.dependentTypes[dependentType] = false
+    local dependentInstance = self.ent[dependentType]
+    self.visuals[dependentInstance] = false
 end
 
 function love.draw()
-    -- Update `order` if it's dirty
-    if orderDirty then
-        order = {}
-        for instance in pairs(all) do
-            table.insert(order, instance)
-        end
-        table.sort(order, function(i1, i2)
-            return i1._depth < i2._depth
-        end)
-    end
+    ensureOrder()
 
     -- Render all visual component types for every visual entity
     for _, instance in ipairs(order) do
-        for dependentType in pairs(instance.dependentTypes) do
-            local dependentInstance = instance.ent[dependentType]
-            if dependentInstance.draw then
-                dependentInstance:draw()
-            end
+        for visual in pairs(instance.visuals) do
+            visual:draw()
         end
     end
 end
