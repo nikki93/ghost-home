d009093049b1b3d0216fcabff1e5c207e5489d41
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-07-06 15:16:02 -0700

    sprite: add `color` prop

--- core/sprite.lua
+++ core/sprite.lua
@@ -7,6 +7,7 @@ local defaultImage = love.graphics.newImage('assets/avatar2.png')
 function Sprite:add()
     self.image = defaultImage
     self.scale = 1
+    self.color = core.color(1, 1, 1, 1)
 
     local spatial = self.Spatial
     spatial.size = core.vec2(self.image:getDimensions())
@@ -15,9 +16,12 @@ end
 function Sprite:draw()
     local spatial = self.Spatial
     local halfSize = spatial.size / 2
+    local prevR, prevG, prevB, prevA = love.graphics.getColor()
+    love.graphics.setColor(self.color.r, self.color.g, self.color.b, self.color.a)
     love.graphics.draw(self.image,
         spatial.position.x, spatial.position.y,
         spatial.rotation,
         self.scale, self.scale,
         halfSize.x, halfSize.y)
+    love.graphics.setColor(prevR, prevG, prevB, prevA)
 end
